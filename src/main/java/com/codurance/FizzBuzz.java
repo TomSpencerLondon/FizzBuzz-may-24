d9b1c2a1903f0709f4d73a6f465d1fb40f5be08d
Tom <thomas.spencer@codurance.com>
2022-05-24 21:35:01 +0100

    Refactor convertIntToFizzBuzz

--- src/main/java/com/codurance/FizzBuzz.java
+++ src/main/java/com/codurance/FizzBuzz.java
@@ -10,22 +10,26 @@ public class FizzBuzz {
 
   public List<String> convert(int start, int end) {
     return IntStream.rangeClosed(start, end).mapToObj(this::convertIntToFizzBuzz)
-        .collect(toList());
+        .toList();
   }
 
   public String convertIntToFizzBuzz(int number) {
+    boolean isDivisibleByFive = number % 5 == 0;
+    boolean isDivisibleByThree = number % 3 == 0;
+
     StringBuilder output = new StringBuilder();
-    if ((number % 5) == 0) {
+
+    if (isDivisibleByFive && isDivisibleByThree) {
+      output.append("FizzBuzz");
+      return output.toString();
+    } else if (isDivisibleByFive) {
       output.append("Buzz");
+      return output.toString();
+    } else if (isDivisibleByThree) {
+      output.append("Fizz");
+      return output.toString();
     }
 
-    if ((number % 3) == 0) {
-      output.insert(0, "Fizz");
-    }
-
-    if (number % 3 != 0 && number % 5 != 0) {
-      output.append(number);
-    }
-    return output.toString();
+    return output.append(number).toString();
   }
 }
