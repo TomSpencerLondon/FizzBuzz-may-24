a68692491f1f3993cd27907a3cc38360ba6544f5
Tom <thomas.spencer@codurance.com>
2022-05-24 22:52:45 +0100

    Extract to static apply(Integer)

--- src/main/java/com/codurance/FizzBuzz.java
+++ src/main/java/com/codurance/FizzBuzz.java
@@ -5,6 +5,17 @@ import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
+  private static String apply(Integer a) {
+    if (a % 5 == 0 && a % 3 == 0) {
+      return "FizzBuzz";
+    } else if (a % 5 == 0) {
+      return "Buzz";
+    } else if (a % 3 == 0) {
+      return "Fizz";
+    } else {
+      return a.toString();
+    }
+  }
 
   public List<String> convert(int start, int end) {
     return IntStream.rangeClosed(start, end).mapToObj(this::convertIntToFizzBuzz)
@@ -12,17 +23,7 @@ public class FizzBuzz {
   }
 
   public String convertIntToFizzBuzz(final int number) {
-    Function<Integer, String> fizzBuzz = (a) -> {
-      if (a % 5 == 0 && a % 3 == 0) {
-        return "FizzBuzz";
-      } else if (a % 5 == 0) {
-        return "Buzz";
-      } else if (a % 3 == 0) {
-        return "Fizz";
-      } else {
-        return a.toString();
-      }
-    };
+    Function<Integer, String> fizzBuzz = FizzBuzz::apply;
 
     return fizzBuzz.apply(number);
   }
